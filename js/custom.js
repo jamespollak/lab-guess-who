51b0fecede3e55aba24ed1df525f0167a1a7bea4
James Pollak <james.pollak@htomail.com>
2019-08-24 15:20:42 +0100

    Finished

---
+++ js/custom.js
@@ -0,0 +1,492 @@
+var cards = [
+  {
+    name: "Alex",
+    img: "Alex.jpg",
+    haircolor: "Black",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No Hat",
+    beard: "No Beard",
+    moustache: "Yes Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Alfred",
+    img: "Alfred.jpg",
+    haircolor: "Ginger",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Blue",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "Yes Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Anita",
+    img: "Anita.jpg",
+    haircolor: "Blond",
+    gender: "Female",
+    glasses: "No Glasses",
+    eyecolor: "Blue",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Anne",
+    img: "Anne.jpg",
+    haircolor: "Black",
+    gender: "Female",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Bernard",
+    img: "Bernard.jpg",
+    haircolor: "Brown",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "Yes Hat",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Bill",
+    img: "Bill.jpg",
+    haircolor: "Ginger",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "Yes Beard",
+    moustache: "No Moustache",
+    bald: "Yes Bald"
+  },
+  {
+    name: "Charles",
+    img: "Charles.jpg",
+    haircolor: "Blond",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "Yes Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Claire",
+    img: "Claire.jpg",
+    haircolor: "Ginger",
+    gender: "Female",
+    glasses: "Yes Glasses",
+    eyecolor: "Brown",
+    hat: "Yes Hat",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "David",
+    img: "David.jpg",
+    haircolor: "Blond",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "Yes Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Eric",
+    img: "Eric.jpg",
+    haircolor: "Blond",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "Yes Hat",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Frans",
+    img: "Frans.jpg",
+    haircolor: "Ginger",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "George",
+    img: "George.jpg",
+    haircolor: "Grey",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "Yes Hat",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Herman",
+    img: "Herman.jpg",
+    haircolor: "Ginger",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "Yes Bald"
+  },
+  {
+    name: "Joe",
+    img: "Joe.jpg",
+    haircolor: "Blond",
+    gender: "Male",
+    glasses: "Yes Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Maria",
+    img: "Maria.jpg",
+    haircolor: "Brown",
+    gender: "Female",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "Yes Hat",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Max",
+    img: "Max.jpg",
+    haircolor: "Black",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "Yes Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Paul",
+    img: "Paul.jpg",
+    haircolor: "Grey",
+    gender: "Male",
+    glasses: "Yes Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Peter",
+    img: "Peter.jpg",
+    haircolor: "Grey",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Blue",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Philip",
+    img: "Philip.jpg",
+    haircolor: "Black",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "Yes Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Richard",
+    img: "Richard.jpg",
+    haircolor: "Brown",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "Yes Beard",
+    moustache: "Yes Moustache",
+    bald: "Yes Bald"
+  },
+  {
+    name: "Robert",
+    img: "Robert.jpg",
+    haircolor: "Brown",
+    gender: "Male",
+    glasses: "No Glasses",
+    eyecolor: "Blue",
+    hat: "No",
+    beard: "Yes Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Sam",
+    img: "Sam.jpg",
+    haircolor: "Grey",
+    gender: "Male",
+    glasses: "Yes Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "Yes Bald"
+  },
+  {
+    name: "Susan",
+    img: "Susan.jpg",
+    haircolor: "Grey",
+    gender: "Female",
+    glasses: "No Glasses",
+    eyecolor: "Brown",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "No Bald"
+  },
+  {
+    name: "Tom",
+    img: "Tom.jpg",
+    haircolor: "Black",
+    gender: "Male",
+    glasses: "Yes Glasses",
+    eyecolor: "Blue",
+    hat: "No",
+    beard: "No Beard",
+    moustache: "No Moustache",
+    bald: "Yes Bald"
+  }
+];
+
+class Game {
+  constructor(cards) {
+    this.allCards = cards;
+    this.cardToGuess;
+    this.computersTurn = false;
+    this.computerCardToGuess = this.getRandomCard();
+    this.setupGame();
+  }
+  setupGame() {
+    //randomly pick a card from this.cards
+    this.cardToGuess = this.getRandomCard();
+    // this.computerCardToGuess;
+    //display that card's back.
+    let html =
+      '<div class="card" data-card-name="' +
+      this.cardToGuess.name +
+      '" haircolor="' +
+      this.cardToGuess.haircolor +
+      '" gender="' +
+      this.cardToGuess.gender +
+      '" glasses="' +
+      this.cardToGuess.glasses +
+      '" eyecolor="' +
+      this.cardToGuess.eyecolor +
+      '" hat="' +
+      this.cardToGuess.hat +
+      '" beard="' +
+      this.cardToGuess.beard +
+      '" moustache="' +
+      this.cardToGuess.moustache +
+      '" bald="' +
+      this.cardToGuess.bald +
+      '">';
+    html += '  <div class="back" name="' + this.cardToGuess.img + '"></div>';
+    html +=
+      '  <div class="front" style="background-image: url(../img/back.jpg' +
+      ')"></div>';
+    html += "</div>";
+
+    $("#guesswho_card").html(html);
+  }
+
+  getRandomCard() {
+    const randomI = Math.floor(Math.random() * this.allCards.length);
+    return this.allCards[randomI];
+  }
+}
+
+let game = newGame();
+function newGame() {
+  shuffleArray(cards);
+  render();
+  return new Game(cards);
+}
+
+$(document).ready(function() {
+  $("button").on("click", function() {
+    const feature = this.getAttribute("feature");
+    const featureValue = this.getAttribute("value");
+    const isFeatureCorrect = game.cardToGuess[feature] === featureValue;
+    // if (!isFeatureCorrect) {
+    //   $(`[${feature}=${featureValue}]`).each(function() {
+    //     $(this)
+    //       .find(".front")
+    //       .css("background-image", "url(../img/back.jpg");
+    //   });
+    // }
+
+    if (isFeatureCorrect) {
+      $("#guesswho_board .card").each(function() {
+        if (this.getAttribute(feature) !== featureValue) {
+          this.setAttribute("available", false);
+          $(this)
+            .find(".front")
+            .css("background-image", "url(../img/back.jpg");
+        }
+      });
+    } else {
+      $("#guesswho_board .card").each(function() {
+        if (this.getAttribute(feature) === featureValue) {
+          this.setAttribute("available", false);
+          $(this)
+            .find(".front")
+            .css("background-image", "url(../img/back.jpg");
+        }
+      });
+    }
+    setTimeout(computerGuessCard, 1000);
+  });
+
+  // function shuffle(cards) {
+  // //   let randomCards = [];
+  // //   for (var i = 0; i < cards.length; i++) {
+  // //     randomCards = Math.floor(Math.random() * cards.length);
+  // //     debugger;
+  // //   }
+  // //   return randomCards;
+  // // }
+});
+
+function shuffleArray(cards) {
+  for (let i = cards.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = cards[i];
+    if (i !== j) {
+      cards[i] = cards[j];
+    }
+    cards[j] = temp;
+  }
+}
+
+function render() {
+  var html = "";
+  cards.forEach(function(pic) {
+    html +=
+      '<div class="card" data-card-name="' +
+      pic.name +
+      '" haircolor="' +
+      pic.haircolor +
+      '" gender="' +
+      pic.gender +
+      '" glasses="' +
+      pic.glasses +
+      '" eyecolor="' +
+      pic.eyecolor +
+      '" hat="' +
+      pic.hat +
+      '" beard="' +
+      pic.beard +
+      '" moustache="' +
+      pic.moustache +
+      '" bald="' +
+      pic.bald +
+      '">';
+    html += '<div class="back" name="' + pic.img + '"></div>';
+    html +=
+      '<div class="front" style="background-image: url(../img/' +
+      pic.img +
+      ')"></div>';
+    html += "</div>";
+  });
+  document.querySelector("#guesswho_board").innerHTML = html;
+  let myScore = parseInt($("#myscore").text());
+  let winAudio = new Audio("/sounds/applause.mp3");
+  $("#guesswho_board .card").on("click", function() {
+    debugger;
+    // let cardClicked;
+    // game.cardToGuess;
+    if ($(this).data("card-name") === game.cardToGuess.name) {
+      $("#guesswho_card")
+        .find(".front")
+        .css("background-image", `url(../img/${game.cardToGuess.img})`);
+      myScore++;
+      $("#myscore").text(myScore);
+      winAudio.play();
+      setTimeout(() => {
+        game = newGame();
+      }, 2000);
+    } else {
+      this.setAttribute("available", false);
+      $(this)
+        .find(".front")
+        .css("background-image", "url(../img/back.jpg");
+      setTimeout(computerGuessCard, 1000);
+    }
+  });
+}
+
+function computerGuessCard() {
+  $(".overlay").css("display", "flex");
+  setTimeout(() => {
+    const availableCards = $("#guesswho_board .card[available!='false']");
+    const randomCardIndex = Math.floor(Math.random() * availableCards.length);
+    const randomCard = availableCards[randomCardIndex];
+    const randaomCardName = randomCard.getAttribute("data-card-name");
+    let computerScore = parseInt($("#computerscore").text());
+    let loseAudio = new Audio("/sounds/boohiss.mp3");
+    if (randaomCardName === game.cardToGuess.name) {
+      $("#guesswho_card")
+        .find(".front")
+        .css("background-image", `url(../img/${game.cardToGuess.img})`);
+      computerScore++;
+      $("#computerscore").text(computerScore);
+      loseAudio.play();
+      setTimeout(() => {
+        game = newGame();
+      }, 2000);
+    } else {
+      randomCard.setAttribute("available", false);
+      $(randomCard)
+        .find(".front")
+        .css("background-image", "url(../img/back.jpg");
+    }
+    $(".overlay").css("display", "none");
+  }, 2000);
+}
+
+/* 
+  after user click card or question show div
+
+*/
